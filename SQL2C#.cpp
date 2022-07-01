0229fa30f843b7bc230b443362cac6faee8445bf
Henrique Bento <68090144+senhorbento@users.noreply.github.com>
2022-07-01 17:23:14 -0300

    Feat: Add mais tipos

--- SQL2C#.cpp
+++ SQL2C#.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 #include <list>
 
 #define _PROPRIEDADE_ " { get; set; }"
 
 using namespace std;
 
+bool VerificarTiposParaString(string verificar){
+    list<string> lista;
+    lista.push_back("VARCHAR");
+    lista.push_back("CHAR");
+    lista.push_back("DATETIME");
+    lista.push_back("DATE");
+    lista.push_back("varchar");
+    lista.push_back("char");
+    lista.push_back("datetime");
+    lista.push_back("date");
+    for(auto tipo : lista){
+        if(verificar.find(tipo) != string::npos)
+            return true;
+    }
+    return false;
+}
+
 int main() {
-    string leitura, mostrar;
+    string leitura, mostrar, variavel;
     list<string> lista;
     size_t pos;
     
@@ -22,24 +40,24 @@ int main() {
 
     for(auto leitura : lista){
         mostrar = leitura;
-        if(mostrar.find("CREATE TABLE") != string::npos){
+        variavel = leitura.substr(0, leitura.find(" "));
+        if(leitura.find("CREATE TABLE") != string::npos){
             pos = mostrar.find("CREATE TABLE");
             mostrar.replace(pos,12,"public class");
-            
             pos = mostrar.find("(");
             mostrar.replace(pos,1,"{");
         }
-        else{
-            string variavel = leitura.substr(0, leitura.find(" "));
-            if(leitura.find("INT") != string::npos){
-                mostrar =  "    public int " + variavel + _PROPRIEDADE_;
-            }
-            if(leitura.find("VARCHAR")!= string::npos){
-                mostrar =  "    public string " + variavel + _PROPRIEDADE_;
-            }
-            if(leitura.find("FLOAT")!= string::npos){
-                mostrar =  "    public float " + variavel + _PROPRIEDADE_;
-            }
+        if(leitura.find("INT")!= string::npos || leitura.find("int")!= string::npos){
+            mostrar =  "    public int " + variavel + _PROPRIEDADE_;
+        }
+        if(leitura.find("FLOAT")!= string::npos || leitura.find("float")!= string::npos){
+            mostrar =  "    public float " + variavel + _PROPRIEDADE_;
+        }
+        if(leitura.find("DOUBLE")!= string::npos || leitura.find("double")!= string::npos){
+            mostrar =  "    public double " + variavel + _PROPRIEDADE_;
+        }
+        if(VerificarTiposParaString(leitura)){
+            mostrar =  "    public string " + variavel + _PROPRIEDADE_;
         }
         cout << mostrar << endl;
     }
